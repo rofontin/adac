221dc16cc3722cc34b7510388f16bb04a359fe27
Riquelmy <fontinriquelmy@gmail.com>
2020-05-06 21:23:45 -0300

    Card

--- lib/TelaMenu.dart
+++ lib/TelaMenu.dart
@@ -13,10 +13,38 @@ class _TelaMenuState extends State<TelaMenu> {
         children: <Widget>[
           Container(
             decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("images/telamenu.png"),
-                fit: BoxFit.cover
-              )
+                image: DecorationImage(
+                    image: AssetImage("images/telamenu.png"),
+                    fit: BoxFit.cover)),
+          ),
+          Center(
+            child: Container(
+              width: 300,
+              height: 100,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                color: Colors.blue,
+                child: InkWell(
+                    splashColor: Colors.white.withAlpha(100),
+                    onTap: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        const ListTile(
+                          leading: Icon(
+                            Icons.add_circle,
+                            size: 70,
+                            color: Colors.white,
+                          ),
+                          title: Text('Novo Texto',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 25)),
+                        ),
+                      ],
+                    )),
+              ),
             ),
           ),
         ],
