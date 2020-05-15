84bfa4b36f5bca09cb06e95142c04bd27f654de1
João Vitor <joaovitor.mil2000@gmail.com>
2020-05-15 19:34:29 -0300

    commit branch Branch-joao

--- lib/TelaMenu.dart
+++ lib/TelaMenu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class TelaMenu extends StatefulWidget {
   @override
@@ -9,44 +10,16 @@ class _TelaMenuState extends State<TelaMenu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: StaggeredGridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
+        padding: EdgeInsets.symmetric(horizontal: 16.0,vertical: 8.0),
         children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/telamenu.png"),
-                    fit: BoxFit.cover)),
-          ),
-          Center(
-            child: Container(
-              width: 300,
-              height: 100,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                color: Colors.blue,
-                child: InkWell(
-                    splashColor: Colors.white.withAlpha(100),
-                    onTap: () {},
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const ListTile(
-                          leading: Icon(
-                            Icons.add_circle,
-                            size: 70,
-                            color: Colors.white,
-                          ),
-                          title: Text('Novo Texto',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 25)),
-                        ),
-                      ],
-                    )),
-              ),
-            ),
-          ),
+          MyItems(Icons.graphic_eq,"Total de visualizações",0xffed622b),
+        ],
+        staggeredTiles: [
+          StaggeredTile.extent(2, 130.0),
         ],
       ),
     );
